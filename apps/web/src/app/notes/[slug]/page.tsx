79d74845f8f441cedc2de0443965b260ce47969b
Vitor Souza <contatovitorsouza@gmail.com>
2025-07-27 02:54:52 +0100

    Fix bad imports for build

--- apps/web/src/app/notes/[slug]/page.tsx
+++ apps/web/src/app/notes/[slug]/page.tsx
@@ -1,8 +1,12 @@
 import Header from "@/components/archived/Header";
-import NoteDetails from "@/components/notes/NoteDetails";
+import NoteDetails from "@/components/archived/notes/NoteDetails";
 import { Id } from "@packages/backend/convex/_generated/dataModel";
 
-export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
   const { slug } = await params;
   return (
     <main className="bg-[#F5F7FE] h-screen">
--- apps/web/src/app/notes/page.tsx
+++ apps/web/src/app/notes/page.tsx
@@ -1,5 +1,5 @@
 import Header from "@/components/archived/Header";
-import Notes from "@/components/notes/Notes";
+import Notes from "@/components/archived/notes/Notes";
 
 export default function Home() {
   return (
